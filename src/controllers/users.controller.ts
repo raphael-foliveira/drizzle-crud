5fd1479da9d977894c42adfa7b835b1c6d01def2
Raphael Oliveira <raphael.foliveira1@gmail.com>
2025-04-20 00:12:32 -0300

    call user repository on user controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,31 +1,16 @@
-import { eq } from 'drizzle-orm';
 import { RouteHandler } from 'fastify';
-import { database } from '../database';
-import { users, tasks, UserInsert } from '../schemas';
+import { usersRepository } from '../repositories';
+import { UserInsert } from '../schemas';
 
 export const getUsers: RouteHandler = async (_, res) => {
-  const result = await database
-    .select()
-    .from(users)
-    .leftJoin(tasks, eq(users.id, tasks.userId));
-
-  const usersResult = result.reduce<Record<string, any>>((acc, row) => {
-    if (!acc[row.users.id]) {
-      acc[row.users.id] = { ...row.users, tasks: [] };
-    }
-    acc[row.users.id].tasks.push(row.tasks);
-    return acc;
-  }, {});
-  return res.status(200).send(Object.values(usersResult));
+  const result = await usersRepository.getUsers();
+  return res.status(200).send(result);
 };
 
 export const createUser: RouteHandler<{ Body: UserInsert }> = async (
   { body },
-  res,
+  res
 ) => {
-  const [user] = await database
-    .insert(users)
-    .values(body)
-    .returning({ id: users.id, email: users.email });
+  const user = await usersRepository.createUser(body);
   return res.status(201).send(user);
 };
